sama152000/Computers-and-Information-En

refactor(routes): convert routes to use lazy-loaded standalone components
- Updated all route definitions to use loadComponent with dynamic imports
- Replaced direct component references with asynchronous component loading
- Added wildcard route to redirect unknown paths to home
- Enabled better code splitting and faster initial load

refactor(news): replace specific types with any in news component

- Changed NewsArticle and NewsItem types to any for flexibility
- Updated method signatures and variables accordingly
- Maintained functionality for fetching and displaying news items

refactor(programs): replace HomeProgram type with any

- Changed programs array type to any[]
- Updated constructor formatting and method code style
- Improved observer callback formatting

refactor(about): replace specific types with any in multiple components

- Removed model imports for AdministrativeMember, DeanMessage, Objective, VisionMission
- Changed related properties to use any or any[] types in AdminStructure, DeanMessage,
  Objectives, VisionMissionpage components
- Preserved existing logic and service injections

refactor(contact-us): replace typed models with any types

- Removed ContactInfo, SocialLink, DepartmentContact model imports
- Updated properties to any or any[] in ContactUsComponent
- Adjusted service subscription formatting for consistency

refactor(department-content): remove explicit model types

- Changed department and currentSection inputs to any type
- Cleaned up imports and formatting in DepartmentContentComponent

refactor(departments): replace types with any and improve formatting

- Removed Department and DepartmentSection model imports
- Updated properties and method parameters to any type
- Applied consistent arrow function formatting across subscriptions and loops
- Added missing trailing commas and corrected imports spacing

refactor(news-details): replace NewsItem type with any

- Removed NewsItem model import
- Adjusted all occurrences to use any 

src/environments/environment.prod.ts
@@ -1,0 +1,11 @@
+/**
+ * Production Environment Configuration
+ * This file contains all environment-specific settings for production
+ */
+export const environment = {
+  production: true,
+  apiUrl: 'https://alisoncollegeen.runasp.net/api/v1',
+  apiTimeout: 30000, // 30 seconds
+  enableDebugMode: false,
+  cacheExpiration: 600000, // 10 minutes
+};

sama152000/Faculty-of-Education-AR

src/environments/environment.prod.ts
@@ -1,0 +1,11 @@
+/**
+ * Production Environment Configuration
+ * This file contains all environment-specific settings for production
+ */
+export const environment = {
+  production: true,
+  apiUrl: 'https://educationar.runasp.net/api/v1',
+  apiTimeout: 30000, // 30 seconds
+  enableDebugMode: false,
+  cacheExpiration: 600000, // 10 minutes
+};